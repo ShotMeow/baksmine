7d49c667e92b9a6d6ef8d7f56dfb5a4ea9c9d312
ShotMeow <shotmeow@gmail.com>
2024-08-13 13:24:31 +0300

    Added: dynamic light & hud pages

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -1,40 +1,48 @@
 export const navItems = [
-    {
-        href: "/",
-        title: "Главная",
-    },
-    {
-        href: "/about-us",
-        title: "О нас",
-    },
-    {
-        href: "/rules",
-        title: "Правила",
-    },
-    {
-        href: "/wiki",
-        title: "Вики",
-    },
+  {
+    href: "/",
+    title: "Главная",
+  },
+  {
+    href: "/about-us",
+    title: "О нас",
+  },
+  {
+    href: "/rules",
+    title: "Правила",
+  },
+  {
+    href: "/wiki",
+    title: "Вики",
+  },
 ];
 
 export const wikiItems = [
-    {
-        href: '/wiki',
-        title: "Введение"
-    },
-    {
-        href: "/wiki/faq",
-        title: "Часто задаваемые вопросы"
-    },
-    {
-        href: "/wiki/commands",
-        title: "Команды"
-    }
-]
+  {
+    href: "/wiki",
+    title: "Введение",
+  },
+  {
+    href: "/wiki/faq",
+    title: "Часто задаваемые вопросы",
+  },
+  {
+    href: "/wiki/commands",
+    title: "Команды",
+  },
+];
 
 export const featuresItems = [
-    {
-        href: "/wiki/voice",
-        title: "Голосовой чат"
-    }
-]
+  {
+    href: "/wiki/voice",
+    title: "Голосовой чат",
+  },
+  {
+    href: "/wiki/dynamic-light",
+    title: "Динамическое освещение",
+  },
+  {
+    href: "/wiki/hud",
+    title: "Переработанный HUD",
+  },
+];
