322db6e83f7c590d315b27df4321f27082357940
ShotMeow <shotmeow@gmail.com>
2024-08-13 14:25:37 +0300

    Fixed: Carousel buttons, home page button

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
 "use client";
-import type {FC} from "react";
-import {AuroraBackground} from "@/features/theme/components/AuroraBackground";
-import {Gamepad} from "lucide-react";
-import {Button} from "@/features/theme/components/MovingBorder";
-import {TextGenerateEffect} from "@/features/theme/components/TextGenerateEffect";
+import type { FC } from "react";
+import { AuroraBackground } from "@/features/theme/components/AuroraBackground";
+import { Gamepad } from "lucide-react";
+import { Button } from "@/features/theme/components/MovingBorder";
+import { TextGenerateEffect } from "@/features/theme/components/TextGenerateEffect";
 
 const Home: FC = () => {
-    return (
-        <main className="container flex h-screen flex-col items-center justify-center text-center">
-            <AuroraBackground className="absolute inset-x-0 top-0 -z-50 animate-show"/>
-            <h1 className="font-montserrat text-3xl font-bold sm:text-6xl lg:text-8xl">
-                <TextGenerateEffect words={"BAKSMINE"} duration={2}/>
-            </h1>
-            <p className="relative mt-4 text-lg sm:text-xl md:mt-10 dark:text-zinc-300">
-                <TextGenerateEffect
-                    words={
-                        "Приватный сервер для классического выживания в комфортной атмосфере."
-                    }
-                />
-            </p>
-            <div className="mt-10 flex animate-show items-center gap-8 md:mt-20">
-                <Button>
-                    <Gamepad/>
-                    Купить проходку
-                </Button>
-            </div>
-        </main>
-    );
+  return (
+    <main className="container flex h-screen flex-col items-center justify-center text-center">
+      <AuroraBackground className="absolute inset-x-0 top-0 -z-50 animate-show" />
+      <h1 className="font-montserrat text-3xl font-bold sm:text-6xl lg:text-8xl">
+        <TextGenerateEffect words={"BAKSMINE"} duration={2} />
+      </h1>
+      <p className="relative mt-4 text-lg dark:text-zinc-300 sm:text-xl md:mt-10">
+        <TextGenerateEffect
+          words={
+            "Приватный сервер для классического выживания в комфортной атмосфере."
+          }
+        />
+      </p>
+      <div className="mt-10 animate-show md:mt-20">
+        <Button>
+          <Gamepad />
+          Купить проходку
+        </Button>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
--- src/features/theme/components/MovingBorder.tsx
+++ src/features/theme/components/MovingBorder.tsx
@@ -1,132 +1,138 @@
 "use client";
-import React, {useRef} from "react";
-import {motion, useAnimationFrame, useMotionTemplate, useMotionValue, useTransform,} from "framer-motion";
+import React, { useRef } from "react";
+import {
+  motion,
+  useAnimationFrame,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import clsx from "clsx";
 
 export function Button({
-                           borderRadius = "1.75rem",
-                           children,
-                           as: Component = "button",
-                           containerClassName,
-                           borderClassName,
-                           duration,
-                           className,
-                           ...otherProps
-                       }: {
-    borderRadius?: string;
-    children: React.ReactNode;
-    as?: any;
-    containerClassName?: string;
-    borderClassName?: string;
-    duration?: number;
-    className?: string;
-    [key: string]: any;
+  borderRadius = "1.75rem",
+  children,
+  as: Component = "button",
+  containerClassName,
+  borderClassName,
+  duration,
+  className,
+  ...otherProps
+}: {
+  borderRadius?: string;
+  children: React.ReactNode;
+  as?: any;
+  containerClassName?: string;
+  borderClassName?: string;
+  duration?: number;
+  className?: string;
+  [key: string]: any;
 }) {
-    return (
-        <Component
+  return (
+    <Component
+      className={clsx(
+        "relative h-16 w-56 overflow-hidden bg-transparent p-px text-xl ",
+        containerClassName,
+      )}
+      style={{
+        borderRadius: borderRadius,
+      }}
+      {...otherProps}
+    >
+      <div
+        className="absolute inset-0"
+        style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
+      >
+        <MovingBorder duration={duration} rx="30%" ry="30%">
+          <div
             className={clsx(
-                "relative h-16 w-56 overflow-hidden bg-transparent p-px text-xl ",
-                containerClassName,
+              "size-20 bg-[radial-gradient(var(--purple-500)_40%,transparent_60%)] opacity-80",
+              borderClassName,
             )}
-            style={{
-                borderRadius: borderRadius,
-            }}
-            {...otherProps}
-        >
-            <div
-                className="absolute inset-0"
-                style={{borderRadius: `calc(${borderRadius} * 0.96)`}}
-            >
-                <MovingBorder duration={duration} rx="30%" ry="30%">
-                    <div
-                        className={clsx(
-                            "size-20 bg-[radial-gradient(var(--purple-500)_40%,transparent_60%)] opacity-80",
-                            borderClassName,
-                        )}
-                    />
-                </MovingBorder>
-            </div>
+          />
+        </MovingBorder>
+      </div>
 
-            <div
-                className={clsx(
-                    "relative flex size-full items-center justify-center gap-2 border border-slate-800 bg-slate-900/[0.8] text-base font-medium text-white antialiased backdrop-blur-xl",
-                    className,
-                )}
-                style={{
-                    borderRadius: `calc(${borderRadius} * 0.96)`,
-                }}
-            >
-                {children}
-            </div>
-        </Component>
-    );
+      <div
+        className={clsx(
+          "relative flex size-full items-center justify-center gap-2 border border-zinc-700 bg-zinc-800/[0.8] text-base font-medium text-white antialiased backdrop-blur-xl",
+          className,
+        )}
+        style={{
+          borderRadius: `calc(${borderRadius} * 0.96)`,
+        }}
+      >
+        {children}
+      </div>
+    </Component>
+  );
 }
 
 export const MovingBorder = ({
-                                 children,
-                                 duration = 2000,
-                                 rx,
-                                 ry,
-                                 ...otherProps
-                             }: {
-    children: React.ReactNode;
-    duration?: number;
-    rx?: string;
-    ry?: string;
-    [key: string]: any;
+  children,
+  duration = 2000,
+  rx,
+  ry,
+  ...otherProps
+}: {
+  children: React.ReactNode;
+  duration?: number;
+  rx?: string;
+  ry?: string;
+  [key: string]: any;
 }) => {
-    const pathRef = useRef<any>();
-    const progress = useMotionValue<number>(0);
+  const pathRef = useRef<any>();
+  const progress = useMotionValue<number>(0);
 
-    useAnimationFrame((time) => {
-        const length = pathRef.current?.getTotalLength();
-        if (length) {
-            const pxPerMillisecond = length / duration;
-            progress.set((time * pxPerMillisecond) % length);
-        }
-    });
+  useAnimationFrame((time) => {
+    const length = pathRef.current?.getTotalLength();
+    if (length) {
+      const pxPerMillisecond = length / duration;
+      progress.set((time * pxPerMillisecond) % length);
+    }
+  });
 
-    const x = useTransform(
-        progress,
-        (val) => pathRef.current?.getPointAtLength(val).x,
-    );
-    const y = useTransform(
-        progress,
-        (val) => pathRef.current?.getPointAtLength(val).y,
-    );
+  const x = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).x,
+  );
+  const y = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).y,
+  );
 
-    const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
+  const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
 
-    return (
-        <>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                className="absolute size-full"
-                width="100%"
-                height="100%"
-                {...otherProps}
-            >
-                <rect
-                    fill="none"
-                    width="100%"
-                    height="100%"
-                    rx={rx}
-                    ry={ry}
-                    ref={pathRef}
-                />
-            </svg>
-            <motion.div
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    display: "inline-block",
-                    transform,
-                }}
-            >
-                {children}
-            </motion.div>
-        </>
-    );
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="absolute size-full"
+        width="100%"
+        height="100%"
+        {...otherProps}
+      >
+        <rect
+          fill="none"
+          width="100%"
+          height="100%"
+          rx={rx}
+          ry={ry}
+          ref={pathRef}
+        />
+      </svg>
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "inline-block",
+          transform,
+        }}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
 };
